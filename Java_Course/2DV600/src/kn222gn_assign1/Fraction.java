0488193a369fd04687906cbb80531690048570bc
KevinUd2014 <kn222gn@student.lnu.se>
2016-09-02 22:02:19 +0200

    added functionality for the fraction.
    Had to look up some fraction on google but went suprisingly good!

--- Java_Course/2DV600/src/kn222gn_assign1/Fraction.java
+++ Java_Course/2DV600/src/kn222gn_assign1/Fraction.java
@@ -2,8 +2,8 @@ package kn222gn_assign1;
 
 public class Fraction {
 	
-	public static int denominator;
-	public static int numerator;
+	public int denominator; // had these static, had an error for HOURS! Static strikes again!
+	public int numerator;
 	
 	public Fraction(int numerator2, int denominator2) {
 		
@@ -19,36 +19,77 @@ public class Fraction {
 		return numerator;
 	}
 	
-	public static boolean isNegative(){
+	public String toString(){
 		
-		boolean boo = false;
-		
-		return boo;
+		return getNumerator() + "/" + getDenominator();
 	}
 	public Fraction add(Fraction newFraction){
 		
-		denominator = this.getDenominator() * newFraction.getDenominator();
-		numerator = this.getNumerator() * newFraction.getDenominator() + newFraction.getNumerator() * this.getDenominator();
-		//and check if simplification is possible.
-		return new Fraction(numerator,denominator);
+		Fraction addedFraction;
+		
+		if(getNumerator() == newFraction.getDenominator()){
+			//returns the new added fraction
+			addedFraction = new Fraction(newFraction.getNumerator() + getNumerator(), getDenominator());
+		}
+		else{
+			//returns the added numerators and denumerators added
+			addedFraction = new Fraction(getNumerator() * newFraction.getDenominator() + newFraction.getNumerator() * getDenominator(), 
+					getDenominator() * newFraction.getDenominator());
+		}
+		
+		return addedFraction;
 	}
-	/*public static int subtract(){
+	public Fraction subtract(Fraction newFraction){
 		
+		Fraction subtractedFraction;
 		
+		if(getNumerator() == newFraction.getDenominator()){
+			//returns the new subtracted fraction
+			subtractedFraction = new Fraction(newFraction.getNumerator() - getNumerator(), getDenominator());
+		}
+		else{
+			//returns the subtracted numerators and denumerators = -
+			subtractedFraction = new Fraction(getNumerator() * newFraction.getDenominator() - newFraction.getNumerator() * getDenominator(), 
+					getDenominator() * newFraction.getDenominator());
+		}
+		
+		return subtractedFraction;
 	}
-	public static int multiply(){
+	public Fraction multiply(Fraction newFraction){
 		
+		numerator = getNumerator() * newFraction.getNumerator();
+		denominator = getDenominator() * newFraction.getDenominator();
+		
+		return new Fraction(numerator, denominator);
 	}
-	public static int divide(){
+	public Fraction divide(Fraction newFraction){
 		
+		Fraction frac;
+		
+		int newDenominator = getDenominator();
+		int newNumerator = getNumerator();
+		
+		//creates a new fraction with the new values
+		frac = new Fraction((newNumerator * newFraction.getDenominator()), 
+				(newDenominator * newFraction.getNumerator()));
+		
+		return frac;
 	}
-	public static int isEqualTo(){
+	public boolean isEqualTo(Fraction newFraction){
 		
+		boolean equalFraction;
 		
+		equalFraction = getDenominator() * newFraction.getNumerator() == getNumerator() * newFraction.getDenominator();
+		
+		return equalFraction;
 	}
-	public static int toString(){
+	public boolean isNegative(){
 		
-		
-	}*/
-
+		if((double)getNumerator() / (double)getDenominator() < 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
--- Java_Course/2DV600/src/kn222gn_assign1/FractionMain.java
+++ Java_Course/2DV600/src/kn222gn_assign1/FractionMain.java
@@ -6,13 +6,58 @@ public class FractionMain {
 	public static void main(String args[]){
 		
 		Fraction frac = new Fraction(2,6);
-		Fraction fracTwo = new Fraction(3,7);
+		Fraction fracTwo = new Fraction(3,4);
 		
-		System.out.print("Add function: ");
+		Fraction fracEqual = new Fraction(5,6);
+		Fraction fracEqualTwo = new Fraction(5,6);
+		
+		System.out.print("Add function: \n\n");
 		System.out.print("");
 		
-		//Fraction addFraction = 
+		System.out.println("Fraction 1: " + frac.toString());
+		System.out.println("Fraction 2: " + fracTwo.toString());
 		
+		System.out.println("Fraction equal 3: " + fracEqual.toString());
+		System.out.println("Fraction equal 4: " + fracEqualTwo.toString());
 		
+		Fraction addFraction = frac.add(fracTwo); // adds the fraction 1 to the fraction 2
+		Fraction subFraction = frac.subtract(fracTwo);//subtracts the fraction 1 to the fraction 2
+		Fraction multiplyFraction = frac.multiply(fracTwo); // multiply the fraction 1 to the fraction 2
+		Fraction dividedFraction = frac.divide(fracTwo);//divides the fractions to eachother
+		
+		System.out.println("\nAdded fractions: ");
+		System.out.println("answer = ");
+		System.out.print("Add: ");
+		System.out.print(addFraction.toString());
+		
+		System.out.println("\n\nNegative added fractions: ");
+		System.out.println("answer = fraction 1(2,6), fraction 2(3,4) = false not negative");
+		System.out.print("Negative: ");
+		System.out.print(addFraction.isNegative());
+		
+		System.out.println("\n\nSubtracted fractions: ");
+		System.out.println("answer = Subtracion between 2 fractions = -something");
+		System.out.print("Subtracted: ");
+		System.out.print(subFraction.toString());
+		
+		System.out.println("\n\nNegative subtraction fractions: ");
+		System.out.println("answer = is negative = true");
+		System.out.print("Subtracted: ");
+		System.out.print(subFraction.isNegative());
+		
+		System.out.println("\n\nMulti fractions: ");
+		System.out.println("answer = multiplied fractions, fraction 1(2,6), fraction 2(3,4) = fraction new(6,24)");
+		System.out.print("multi: ");
+		System.out.print(multiplyFraction.toString());
+		
+		System.out.println("\n\ndivided fractions: ");
+		System.out.println("answer = divided fractions");
+		System.out.print("Divided: ");
+		System.out.print(dividedFraction.toString());
+		
+		System.out.println("\n\nisEqual fractions: ");
+		System.out.println("answer = Fraction 1(5,6), Fraction 2(5,6) = isEqual = true");
+		System.out.print("isEqual: ");
+		System.out.print(fracEqual.isEqualTo(fracEqualTwo));//true
 	}
 }
