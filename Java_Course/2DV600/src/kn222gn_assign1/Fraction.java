e6f1b5adb0a212566229beff00b8dcee5941c324
KevinUd2014 <kn222gn@student.lnu.se>
2016-09-02 18:13:46 +0200

    added fraction

--- Java_Course/2DV600/src/kn222gn_assign1/Fraction.java
+++ Java_Course/2DV600/src/kn222gn_assign1/Fraction.java
@@ -1,5 +1,54 @@
 package kn222gn_assign1;
 
 public class Fraction {
+	
+	public static int denominator;
+	public static int numerator;
+	
+	public Fraction(int numerator2, int denominator2) {
+		
+		numerator = numerator2;
+		denominator = denominator2;
+	}
+	public int getDenominator(){
+		
+		return denominator;
+	}
+	public int getNumerator(){
+		
+		return numerator;
+	}
+	
+	public static boolean isNegative(){
+		
+		boolean boo = false;
+		
+		return boo;
+	}
+	public Fraction add(Fraction newFraction){
+		
+		denominator = this.getDenominator() * newFraction.getDenominator();
+		numerator = this.getNumerator() * newFraction.getDenominator() + newFraction.getNumerator() * this.getDenominator();
+		//and check if simplification is possible.
+		return new Fraction(numerator,denominator);
+	}
+	/*public static int subtract(){
+		
+		
+	}
+	public static int multiply(){
+		
+	}
+	public static int divide(){
+		
+	}
+	public static int isEqualTo(){
+		
+		
+	}
+	public static int toString(){
+		
+		
+	}*/
 
 }
--- Java_Course/2DV600/src/kn222gn_assign1/FractionMain.java
+++ Java_Course/2DV600/src/kn222gn_assign1/FractionMain.java
@@ -5,6 +5,14 @@ public class FractionMain {
 	
 	public static void main(String args[]){
 		
+		Fraction frac = new Fraction(2,6);
+		Fraction fracTwo = new Fraction(3,7);
+		
+		System.out.print("Add function: ");
+		System.out.print("");
+		
+		//Fraction addFraction = 
+		
 		
 	}
 }
