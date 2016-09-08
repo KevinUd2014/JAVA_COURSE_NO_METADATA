68df5a51e7af503720f1cc054b6c7dff0305d4a2
KevinUd2014 <kn222gn@student.lnu.se>
2016-09-08 17:27:26 +0200

    Made some preporation for assign 14

---
+++ Java_Course/2DV600/src/kn222gn_assign1/Queue.java
@@ -0,0 +1,48 @@
+package kn222gn_assign1;
+
+import java.util.Iterator;
+
+public class Queue implements QueueInterface {
+	
+	@Override
+	public int size() {
+		
+		int newSize = 0;
+		
+		return newSize;
+	}
+	
+	@Override
+   public boolean isEmpty() {
+		return false;
+	}
+	
+	@Override
+	public void enqueue(Object element) {
+	}
+	
+	@Override
+	public Object dequeue() throws IndexOutOfBoundsException {
+		return null;
+	}
+	
+	@Override
+	public Object first() throws IndexOutOfBoundsException {
+		return null;
+	}
+
+	@Override
+	public Object last() throws IndexOutOfBoundsException {
+		return null;
+	}
+	
+	@Override
+	public boolean contains(Object o) {
+		return false;
+	}
+	
+	@Override
+	public Iterator iterator() {
+		return null;
+	}
+}
