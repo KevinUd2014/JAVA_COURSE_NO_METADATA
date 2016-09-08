e84b333836e985785d26af4556990e2fd55eeff9
KevinUd2014 <kn222gn@student.lnu.se>
2016-09-08 15:23:53 +0200

    added some lines but looked at the wrong game....

---
+++ Java_Course/2DV600/src/kn222gn_assign1/Play123Main.java
@@ -0,0 +1,26 @@
+package kn222gn_assign1;
+
+public class Play123Main {
+
+	
+	public static void main(String args[]){
+		
+		int win = 0;
+		int timesToPlay = 10000;
+		
+		for(int i = 0; i < timesToPlay; i++){
+			
+			Deck deck = new Deck();
+			deck.shuffle();
+			
+			int countTimes = 1;
+			
+			
+		}
+	}
+	
+	public static boolean playGame(int counter, Deck deck){
+		
+		
+	}
+}
