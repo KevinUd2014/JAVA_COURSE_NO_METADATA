652a08eda89169f60829cd6d76196bb0f81ca2c9
KevinUd2014 <kn222gn@student.lnu.se>
2016-09-19 16:57:24 +0200

    Started working on exercise 4

---
+++ Java_Course/kn222gn_assign2/src/kn222gn_assign2/Exercise4/QueueInterface.java
@@ -0,0 +1,16 @@
+package kn222gn_assign2.Exercise4;
+
+/**
+ * Created by Golde on 2016-09-19.
+ */
+public interface QueueInterface<E> extends Iterable<E> {
+
+
+    int size();                          // current queue size
+    boolean isEmpty();                   // true if queue is empty
+    void enqueue(E element);             // add element at end of queue
+    E dequeue();                         // return and remove first element.
+    E first();                           // return (without removing) first element
+    E last();                            // return (without removing) last element
+
+}
