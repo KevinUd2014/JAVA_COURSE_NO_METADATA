d31c806e33bb5669beb6c3e4aacbee014e7b4b91
KevinUd2014 <kn222gn@student.lnu.se>
2016-09-21 15:32:13 +0200

    added some functions on exercise 5

---
+++ Java_Course/kn222gn_assign2/src/kn222gn_assign2/Exercise5/IdentifyWordsMain.java
@@ -0,0 +1,87 @@
+package kn222gn_assign2.Exercise5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+/**
+ * Created by Golde on 2016-09-21.
+ */
+public class IdentifyWordsMain {
+
+private static String originalText;
+
+    public static void main(String args[]){
+
+        try{
+
+            String path = args[0];//"D:\\new\\Java_course\\JAVA_COURSE_NO_METADATA\\Java_Course\\kn222gn_assign2\\src\\kn222gn_assign2\\Exercise5\\HistoryOfProgramming.txt";
+
+            //Scanner inputScanner = new Scanner(System.in);
+
+            File newFile = new File(path);
+            originalText = readFile(newFile);
+
+            //System.out.println(originalText);
+            createFile(originalText, path);
+        }
+        catch(Exception error){
+
+            System.err.println("Something went wrong when creating the file!");
+        }
+    }
+    public static String readFile(File fileWithTest){
+
+        String content = "";
+        String contentToPrint = "";
+
+        try{
+
+            Scanner scanner = new Scanner(fileWithTest);
+
+            while(scanner.hasNext()){
+
+                content += scanner.nextLine() + "\n";
+            }
+
+            char[] characters = content.toCharArray();
+
+            for(Character character : characters){
+
+                //only do this when these characters are found in the text file
+                if(Character.isLetter(character) || Character.isWhitespace(character)){
+
+                    contentToPrint += character;
+                }
+                scanner.close();
+            }
+        }
+        catch(FileNotFoundException error){
+
+            System.err.println("File not found");
+        }
+        return contentToPrint;
+    }
+    public static void createFile(String originalText, String path){
+
+
+        File file = new File(path);
+
+        try{
+
+            //System.out.println("hello");
+            FileWriter writer = new FileWriter(file.getParentFile().getPath() + "/words" + ".txt");
+            writer.write(originalText);
+            writer.flush();
+            writer.close();
+        }
+        catch(IOException error){
+
+            System.err.println("Error when creating the file");
+        }
+
+    }
+}
---
+++ Java_Course/kn222gn_assign2/src/kn222gn_assign2/Exercise5/Word.java
@@ -0,0 +1,44 @@
+package kn222gn_assign2.Exercise5;
+
+/**
+ * Created by Golde on 2016-09-21.
+ */
+public class Word implements Comparable<Word> {
+
+    private String word;
+
+    public Word(String str) {
+
+        word = str;
+    }
+    public String toString() {
+
+        return word;
+    }
+    /* Override Object methods */
+    public int hashCode() { /*"compute a hash value for word"*/
+
+        int hashCode = 0;
+
+        for(int i = 0; i < word.length(); i+=1){
+
+            char character = word.charAt(i);
+            hashCode += Character.getNumericValue(character);
+        }
+        return  hashCode;
+    }
+    public boolean equals(Object other) { /*"true if two words are equal"*/
+
+        if(other instanceof Word){
+
+            Word otherWord = (Word) other;
+            return word.toLowerCase().equals(otherWord.word.toLowerCase());
+        }
+        return false;
+    }
+    /* Implement Comparable */
+    public int compareTo(Word w) { /*"compares two words lexicographically"*/
+
+        return word.compareToIgnoreCase(w.word);
+    }
+}
